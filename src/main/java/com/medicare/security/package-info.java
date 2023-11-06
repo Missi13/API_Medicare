4052b4bdab18e4e4cbba8f45af687bdfa262cf97
Micipsa <missi.adel@gmail.com>
2023-11-06 14:09:21 -0500

    initialize

---
+++ src/main/java/com/medicare/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package com.medicare.security;
---
+++ src/test/java/com/medicare/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.medicare;
+
+import com.medicare.MedicareApp;
+import com.medicare.config.AsyncSyncConfiguration;
+import com.medicare.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { MedicareApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
